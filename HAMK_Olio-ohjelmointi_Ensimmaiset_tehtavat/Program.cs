8a35c3689aa2515c5a893572e55ce0d24035c4ae
Aleksi Bovellan <aleksi.bovellan@student.hamk.fi>
2022-01-21 02:00:52 +0200

    Ensimmäinen yritys koskaan lisätä tiedostot Githubiin

---
+++ HAMK_Olio-ohjelmointi_Ensimmaiset_tehtavat/Program.cs
@@ -0,0 +1,42 @@
+﻿// Olio-ohjelmointi - Aleksi Bovellan - INTIM21A6
+// Harjoitus 1
+
+
+// Alustuksia
+
+int kierrosLaskuri = 0;
+int kysymysLaskuri = kierrosLaskuri + 1;
+double annaLuku;
+double numerotYhteensa = 0.0;
+
+
+// Kysytaan numeroiden kpl
+
+Console.WriteLine("Terve!\n");
+
+Console.WriteLine("Monenko luvun keskiarvon haluaisit saada?");
+
+int numeroidenMaara;
+int.TryParse(Console.ReadLine(), out numeroidenMaara);
+
+
+// Loopataan tarvittavat numerot talteen
+
+while (kierrosLaskuri != numeroidenMaara)
+{
+    Console.WriteLine("\nAnna {0}. lukusi:", kysymysLaskuri);
+
+    double.TryParse(Console.ReadLine(), out annaLuku);
+
+    numerotYhteensa += annaLuku;
+    kierrosLaskuri++;
+    kysymysLaskuri++;
+}
+
+
+// Lasketaan ja tulostetaan saatujen numeroiden keskiarvo
+
+double numeroidenKeskiarvo = numerotYhteensa / numeroidenMaara;
+
+Console.WriteLine("\nLukujesi keskiarvo oli: {0}", numeroidenKeskiarvo);
+Console.WriteLine("\n");
---
+++ HAMK_Olio-ohjelmointi_Ensimmaiset_tehtavat_2/Program.cs
@@ -0,0 +1,55 @@
+﻿// Olio-ohjelmointi - Aleksi Bovellan - INTIM21A6
+// Harjoitus 2
+
+
+// Alustuksia
+
+string? yhteenVaiVahennys = null;
+double summa;
+
+
+// Kysytään tehdäänkö yhteen- vai vähennyslasku
+
+Console.WriteLine("Terve!\n");
+
+Console.WriteLine("Yhteen- vai vähennyslasku? (+/-)");
+yhteenVaiVahennys = Console.ReadLine();
+
+
+// Tarkistetaan oliko syöte halutun kaltainen - jos ei, niin eteneminen keskeytyy.
+
+if (yhteenVaiVahennys == "+" || yhteenVaiVahennys == "-") {
+
+
+    // Kysytaan tarvittavat numerot talteen
+
+    Console.WriteLine("\nAnna ensimmäinen luku:");
+
+    double ekaLuku;
+    double.TryParse(Console.ReadLine(), out ekaLuku);
+
+
+    Console.WriteLine("Anna toinen luku:");
+
+    double tokaLuku;
+    double.TryParse(Console.ReadLine(), out tokaLuku);
+
+
+    // Tehdään haluttu laskutoimitus ja printataan lopputulos
+
+    if (yhteenVaiVahennys == "+")
+    {
+        summa = ekaLuku + tokaLuku;
+        Console.WriteLine("\nAntamiesi lukujen summa on: " + summa);
+    }
+    else
+    {
+        summa = ekaLuku - tokaLuku;
+        Console.WriteLine("\nAntamiesi lukujen erotus on: " + summa);
+    }
+
+}
+else {
+    // Muuten, näkemiin.
+    Console.WriteLine("\nValintasi ei ollut oikeanlainen. Sorry.\n");
+}
